ff82fb1fd99ff395e8dc6f6171e7ac458e853841
HarshitVashisht11 <harshitvashisht07@gmail.com>
2023-12-23 18:58:52 +0530

    updated navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-end items-center bg-zinc-900 p-4 text-white">
       <a
-        href="https://github.com/HarshitVashisht11"
+        href="https://github.com/HarshitVashisht11/ThalaReviewSystem"
         target="_blank"
         rel="noopener noreferrer"
         className="flex items-center space-x-2"
